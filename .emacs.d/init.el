f9f41bec64bc3c2bcb36000e4b4ad10cb02ce104
Yoshi Yamaguchi <yoshifumi@google.com>
2012-07-25 19:11:42 +0800

    fixed init.el for Linux

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -149,26 +149,27 @@
   (set-face-attribute 'default nil
 					  :family "monaco"
 					  :height 140)
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'japanese-jisx0208
-   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'japanese-jisx0212
-   '("Hiragino Maru Gothic Pro" . "iso10646-1"))
-  (set-fontset-font
-   (frame-parameter nil 'font)
-   'mule-unicode-0100-24ff
-   '("monaco" . "iso10646-1"))
-  (setq face-font-rescale-alist
-		'(("^-apple-hiragino.*" . 1.2)
-		  (".*osaka-bold.*" . 1.2)
-		  (".*osaka-medium.*" . 1.2)
-		  (".*courier-bold-.*-mac-roman" . 1.2)
-		  (".*monaco cy-bold-.*-mac-cyrillic" . 1.0)
-		  (".*monaco-bold-.*-mac-roman" . 0.9)
-		  ("-cdac$" . 1.3))))
+  (when (eq system-type 'darwin)
+	(set-fontset-font
+	 (frame-parameter nil 'font)
+	 'japanese-jisx0208
+	 '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+	(set-fontset-font
+	 (frame-parameter nil 'font)
+	 'japanese-jisx0212
+	 '("Hiragino Maru Gothic Pro" . "iso10646-1"))
+	(set-fontset-font
+	 (frame-parameter nil 'font)
+	 'mule-unicode-0100-24ff
+	 '("monaco" . "iso10646-1"))
+	(setq face-font-rescale-alist
+		  '(("^-apple-hiragino.*" . 1.2)
+			(".*osaka-bold.*" . 1.2)
+			(".*osaka-medium.*" . 1.2)
+			(".*courier-bold-.*-mac-roman" . 1.2)
+			(".*monaco cy-bold-.*-mac-cyrillic" . 1.0)
+			(".*monaco-bold-.*-mac-roman" . 0.9)
+			("-cdac$" . 1.3)))))
 
 (when (< emacs-major-version 23)
   (when (eq system-type 'darwin)
