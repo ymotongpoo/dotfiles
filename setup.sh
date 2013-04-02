3ec637ff2d891256064aed4e6259e385e7e90dce
ymotongpoo <ymotongpoo@gmail.com>
2013-04-02 16:26:02 +0900

    added setup file

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# This directory is supporsed to be in $HOME as .dotfiles
+DOTFILES="$HOME"/.dotfiles
+cd "$HOME"
+
+# set zshell configuration files
+ln -s "$DOTFILES"/.zshrc .zshrc
+platform=`uname`
+if [ platform = "Linux" ]; then
+  ln -s "$DOTFILES"/.zshenv.linux .zshenv
+elif [ platform = "Darwin" ]; then
+  ln -s "$DOTFILES"/.zshenv.macosx .zshenv
+fi
+
+# set editor configuration files
+ln -s "$DOTFILES"/.emacs.d .emacs.d
+ln -s "$DOTFILES"/.vim .vim
+
+# set terminal multiplexer configuration files
+ln -s "$DOTFILES"/.tmux.conf .tmux.conf
+ln -s "$DOTFILES"/.screenrc .screenrc
