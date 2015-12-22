26311ce0da5e7362a6a3c047fbf9610e911d63b8
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2015-12-22 12:40:00 +0900

    splitted .tmux.conf into linux and osx version respectively

--- setup.sh
+++ setup.sh
@@ -17,13 +17,31 @@ ln -s "$DOTFILES"/.emacs.d .emacs.d
 ln -s "$DOTFILES"/.vim .vim
 
 # set terminal multiplexer configuration files
-ln -s "$DOTFILES"/.tmux.conf .tmux.conf
 ln -s "$DOTFILES"/.screenrc .screenrc
+if [ platform = "Linux" ]; then
+  ln -s "$DOTFILES"/.tmux.conf.linux .tmux.conf
+elif [ platform = "Darwin" ]; then
+  ln -s "$DOTFILES"/.tmux.conf.macosx .tmux.conf
+fi
 
 # create private bin directory
 mkdir "$HOME"/bin
 
-cd "$HOME"/bin
+# Download and install useful tools
+tmpdir="$HOME/bin/tmp"
+mkdir "$tmpdir"
+cd "$tmpdir"
+
+if [ platform = "Linux" ]; then
+  wget "https://github.com/monochromegane/the_platinum_searcher/releases/download/v2.0.2/pt_linux_amd64.tar.gz"
+  wget "https://github.com/peco/peco/releases/download/v0.3.5/peco_linux_amd64.tar.gz"
+  wget "https://drive.google.com/uc?id=0B3X9GlR6Embnb095MGxEYmJhY2c"
+elif [ platform = "Darwin" ]; then
+  curl -O "https://github.com/monochromegane/the_platinum_searcher/releases/download/v2.0.2/pt_linux_amd64.tar.gz"
+  curl -O "https://github.com/peco/peco/releases/download/v0.3.5/peco_darwin_amd64.zip"
+  curl -O -L "https://drive.google.com/uc?id=0B3X9GlR6EmbnVjIzMDRqck1aekE" --output drive
+fi
+
 
 # setup OPAM
 # wget http://www.ocamlpro.com/pub/opam_installer.sh
