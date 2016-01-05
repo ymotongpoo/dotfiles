95f65a6966c3530104dcac5821672ddb27930703
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2016-01-05 17:41:20 +0900

    deleted archive-contents from repo and add some setting for golang in setup.sh

--- .emacs.d/inits/20_golang.el
+++ .emacs.d/inits/20_golang.el
@@ -1,13 +1,29 @@
 (message "20. Go programming language settings")
+
+(defvar my-gopath)
+(setq my-gopath (concat (getenv "HOME") "src/go/workspace"))
+(setenv "GOPATH" my-gopath)
+(add-to-list 'exec-path (concat my-gopath "/bin"))
+
 (add-hook 'go-mode-hook
           (lambda ()
             (setq c-basic-offset 4)
-            (setq indent-tabs-mode t)
-            (local-set-key (kbd "M-.") 'godef-jump)
-            (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-            (local-set-key (kbd "C-c i") 'go-goto-imports)
-            (local-set-key (kbd "C-c d") 'godoc)))
-(add-hook 'before-save-hook 'gofmt-before-save)
+            (setq indent-tabs-mode t)))
+
+(defun my-go-mode-hook ()
+  (add-hook 'before-save-hook 'gofmt-before-save)
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go-generate && go build -v && go test -v && go vet"))
+  (local-set-key (kbd "M-.") 'godef-jump)
+  (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
+  (local-set-key (kbd "C-c i") 'go-goto-imports)
+  (local-set-key (kbd "C-c d") 'godoc))
+
+(add-hook 'go-mode-hook 'my-go-mode-hook)
+
+(require 'go-autocomplete)
+(require 'auto-complete-config)
 
 ;;(define-key go-mode-map (kbd "C-c C-j") 'go-direx-pop-to-buffer)
 ;;(push '(direx:direx-mode :position left :width 0.4 :dedicated t :stick t)
--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 DOTFILES="$HOME"/.dotfiles
 cd "$HOME"
 
-# set zshell configuration files
+##### set zshell configuration files
 ln -s "$DOTFILES"/.zshrc .zshrc
 platform=`uname`
 if [ platform = "Linux" ]; then
@@ -12,11 +12,11 @@ elif [ platform = "Darwin" ]; then
   ln -s "$DOTFILES"/.zshenv.macosx .zshenv
 fi
 
-# set editor configuration files
+##### set editor configuration files
 ln -s "$DOTFILES"/.emacs.d .emacs.d
 ln -s "$DOTFILES"/.vim .vim
 
-# set terminal multiplexer configuration files
+##### set terminal multiplexer configuration files
 ln -s "$DOTFILES"/.screenrc .screenrc
 if [ platform = "Linux" ]; then
   ln -s "$DOTFILES"/.tmux.conf.linux .tmux.conf
@@ -24,23 +24,34 @@ elif [ platform = "Darwin" ]; then
   ln -s "$DOTFILES"/.tmux.conf.macosx .tmux.conf
 fi
 
-# create private bin directory
+##### create private bin directory
 mkdir "$HOME"/bin
 
-# Download and install useful tools
+##### Download and install useful tools
 tmpdir="$HOME/bin/tmp"
 mkdir "$tmpdir"
 cd "$tmpdir"
 
-if [ platform = "Linux" ]; then
+if [ $platform = "Linux" ]; then
   wget "https://github.com/monochromegane/the_platinum_searcher/releases/download/v2.0.2/pt_linux_amd64.tar.gz"
   wget "https://github.com/peco/peco/releases/download/v0.3.5/peco_linux_amd64.tar.gz"
   wget "https://drive.google.com/uc?id=0B3X9GlR6Embnb095MGxEYmJhY2c"
-elif [ platform = "Darwin" ]; then
+elif [ $platform = "Darwin" ]; then
   curl -O "https://github.com/monochromegane/the_platinum_searcher/releases/download/v2.0.2/pt_linux_amd64.tar.gz"
   curl -O "https://github.com/peco/peco/releases/download/v0.3.5/peco_darwin_amd64.zip"
   curl -O -L "https://drive.google.com/uc?id=0B3X9GlR6EmbnVjIzMDRqck1aekE" --output drive
 fi
+
+##### set Go workspace
+goworkspace="$HOME/src/go/workspace"
+mkdir -p "$goworkspace"
+cd "$goworkspace"
+export GOPATH="$goworkspace"
+
+# go get -u golang.org/x/tools/cmd/goimports
+# go get -u golang.org/x/tools/cmd/oracle
+# go get -u github.com/rogpeppe/godef
+# go get -u github.com/nsf/gocode
 
 
 # setup OPAM
