e356307f03241b69658a98613851591a6a7eab5f
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2017-11-01 12:32:57 +0900

    set up for TypeScript and OCaml

---
+++ .emacs.d/inits/23_ocaml.el
@@ -0,0 +1,14 @@
+(require 'tuareg)
+(add-hook 'tuareg-mode-hook
+          (lambda ()
+            (setq tuareg-in-indent 0)
+            (setq electric-indent-mode nil)
+            (setq tuareg-leading-star-in-doc t)
+            (setq tuareg-with-indent 0)))
+
+(require 'merlin)
+; Make company aware of merlin
+(with-eval-after-load 'company
+  (add-to-list 'company-backends 'merlin-company-backend))
+; Enable company on merlin managed buffers
+(add-hook 'merlin-mode-hook 'company-mode)
