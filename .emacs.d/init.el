f6c5532f39cba352d647a900f69032555a92c2fe
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2017-11-02 15:26:32 +0900

    add ocp-indent for OCaml settings and changed theme to monokai

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -21,7 +21,7 @@
 (tool-bar-mode -1)
 
 ;;;;; theme
-(load-theme 'sanityinc-tomorrow-eighties t)
+(load-theme 'monokai t)
 
 ;;;;; highlight
 (setq hl-line-face 'underline)
@@ -44,22 +44,13 @@
 ;; auto revert buffer, useful for update of VCS repo
 (global-auto-revert-mode 1)
 
+;; change backup file location
+(setq backup-directory-alist
+      (cons (cons ".*" (expand-file-name "~/.emacs.d/backup"))
+        backup-directory-alist))
+
 ;;;;;;;;;; init-loader
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
 (put 'set-goal-column 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (emmet-mode js2-mode yasnippet web-mode use-package smex smartparens recentf-ext projectile prodigy popwin pallet nyan-mode multiple-cursors magit init-loader idle-highlight-mode htmlize go-mode flycheck-cask expand-region exec-path-from-shell drag-stuff company color-theme-sanityinc-tomorrow))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- .emacs.d/inits/23_ocaml.el
+++ .emacs.d/inits/23_ocaml.el
@@ -1,10 +1,12 @@
 (require 'tuareg)
+(require 'ocp-indent)
 (add-hook 'tuareg-mode-hook
           (lambda ()
             (setq tuareg-in-indent 0)
             (setq electric-indent-mode nil)
             (setq tuareg-leading-star-in-doc t)
             (setq tuareg-with-indent 0)))
+
 
 (require 'merlin)
 ; Make company aware of merlin
