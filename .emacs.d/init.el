e1206ac65206654e22092e206247f36e040eae2a
Yoshifumi YAMAGUCHI <ymotongpoo@gmail.com>
2017-11-07 00:17:39 +0900

    moved custom variables in init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,12 +1,10 @@
-;;;;; start Emacs server
-
-
-;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
+;; Added by Package.el.  This must come before configurations of
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
 ;(package-initialize)
 
+;;;;; start Emacs server
 (require 'server)
 (unless (server-running-p)
   (server-start))
@@ -16,32 +14,20 @@
 	("melpa" . "http://melpa.org/packages/")
 	("org" . "http://orgmode.org/elpa/")))
 
-;;;;;;;;;; init-loader
+(require 'package)
+
+;;;;; install required pacakges
+(package-initialize)
+(unless package-archive-contents
+    (package-refresh-contents))
+(package-install-selected-packages) ;; call M-x package-install-selected-packages for first launch
+
+;;;;; init-loader
 (add-to-list 'load-path "~/.emacs.d/elisp")
 (require 'init-loader)
 (init-loader-load "~/.emacs.d/inits")
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (auto-async-byte-compile bind-key company company-go company-web dash emmet-mode exec-path-from-shell expand-region f flycheck go-mode htmlize idle-highlight-mode init-loader js2-mode magit merlin monokai-theme multiple-cursors neotree ocp-indent popwin prodigy projectile recentf-ext s smartparens smex tide tuareg typescript-mode web-mode yasnippet))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(web-mode-comment-face ((t (:foreground "#D9333F"))))
- '(web-mode-css-at-rule-face ((t (:foreground "#FF7F00"))))
- '(web-mode-css-pseudo-class-face ((t (:foreground "#FF7F00"))))
- '(web-mode-css-rule-face ((t (:foreground "#A0D8EF"))))
- '(web-mode-doctype-face ((t (:foreground "#82AE46"))))
- '(web-mode-html-attr-name-face ((t (:foreground "#C97586"))))
- '(web-mode-html-attr-value-face ((t (:foreground "#82AE46"))))
- '(web-mode-html-tag-bracket-face ((t (:foreground "#aaaaaa"))))
- '(web-mode-html-tag-custom-face ((t (:foreground "#FF3366" :weight bold))))
- '(web-mode-html-tag-face ((t (:foreground "#E6B422" :weight bold))))
- '(web-mode-server-comment-face ((t (:foreground "#D9333F")))))
+;;;;; change the place for Custom
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
--- .emacs.d/inits/01_install_selected_packages.el
+++
@@ -1,6 +0,0 @@
-(require 'package)
-
-(package-initialize)
-(unless package-archive-contents
-    (package-refresh-contents))
-(package-install-selected-packages)
