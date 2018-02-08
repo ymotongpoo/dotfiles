8ff98161d07a43e5c75411f721aad43ace7b51c6
Yoshi Yamaguchi <ymotongpoo@gmail.com>
2018-02-08 21:47:37 +0900

    fix go version and emacs theme settings

--- .emacs.d/inits/01_server.el
+++ .emacs.d/inits/01_server.el
@@ -5,30 +5,22 @@
   (unless (server-running-p)
     (server-start))
   (when (display-graphic-p)
-    (add-to-list 'default-frame-alist '(font . "Source Han Code JP R 12"))
-    (set-face-attribute 'default nil :font "Source Han Code JP R 12")))
+    (add-to-list 'default-frame-alist '(font . "Source Han Code JP 12"))
+    (set-face-attribute 'default nil :font "Source Han Code JP 12")))
 
 (if (daemonp)
     (add-hook 'after-make-frame-functions
 	      (lambda (frame)
 		(select-frame frame)
-		(use-package color-theme
-		  :ensure t
-		  :if (display-graphic-p)
-		  :config
 		  (when (display-graphic-p)
-		    (add-to-list 'default-frame-alist '(font . "Source Han Code JP R 12"))
-		    (set-face-attribute 'default nil :font "Source Han Code JP R 12"))
+		    (add-to-list 'default-frame-alist '(font . "Source Han Code JP 12"))
+		    (set-face-attribute 'default nil :font "Source Han Code JP 12"))
 		  (use-package monokai-theme
 		    :ensure t
 		    :config
-		    (load-theme 'monokai t)))))
-  (use-package color-theme
-    :ensure t
-    :if (display-graphic-p)
-    :config
+		    (load-theme 'monokai t))))
     (use-package monokai-theme
       :ensure t
       :config
-      (load-theme 'monokai t))))
+      (load-theme 'monokai t)))
 		  
